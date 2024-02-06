b328b0cb03a1bdba1f57c6d4a1e62d63fd597bf3
joiyeon <whdldus1212@gmail.com>
2024-02-06 20:50:47 +0900

    그리디 2일차

---
+++ src/GreedyAlgorithm/bigNumber.java
@@ -0,0 +1,33 @@
+package GreedyAlgorithm;
+import java.util.*;
+public class bigNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // N, M, K를 공백을 기준으로 구분하여 입력 받기
+        int n = sc.nextInt();   //배열의 크기
+        int m = sc.nextInt();   //숫자 더해지는 횟수
+        int k = sc.nextInt();   //연속해서 k번을 초과하여 더해질 수 없음
+
+        // N개의 수를 공백을 기준으로 구분하여 입력 받기
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr); // 입력 받은 수들 정렬하기
+        int first = arr[n - 1]; // 가장 큰 수
+        int second = arr[n - 2]; // 두 번째로 큰 수
+
+        // 가장 큰 수가 더해지는 횟수 계산
+        int cnt = (m / (k + 1)) * k;
+        cnt += m % (k + 1);
+
+        int result = 0;
+        result += cnt * first; // 가장 큰 수 더하기
+        result += (m - cnt) * second; // 두 번째로 큰 수 더하기
+
+        System.out.println(result);
+    }
+}
